0663f52b3ec5424467720b1362c1c9386cdfc943
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-17 17:24:10 +0100

    completed email_valdoise.rb

--- lib/email_valdoise.rb
+++ lib/email_valdoise.rb
@@ -1,5 +1,35 @@
-# frozen_string_literal = true
+# frozen_string_literal: true
 
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
+
+def url_loader(url)
+  Nokogiri::HTML(open(url))
+end
+
+def page
+  Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+end
+
+def link_fetcher
+  links = page.xpath('//a[@class="lientxt"]/@href').to_a
+  links.map! { |adress| adress.to_s.gsub(/^\./, "http://annuaire-des-mairies.com") }
+end
+
+def mail_fetcher
+  emails = []
+  link_fetcher.each do |link|
+    emails.push(url_loader(link).xpath('//section[2]//tr[4]/td[2]').text.to_s)
+  end
+  emails
+end
+
+def name_fetcher
+  names = page.xpath('//a[@class="lientxt"]/text()').to_a
+  names.map! { |name| name.to_s.downcase.capitalize }
+end
+
+def data_gatherer
+  name_fetcher.zip(mail_fetcher).to_h
+end
--- spec/email_valdoise_spec.rb
+++ spec/email_valdoise_spec.rb
@@ -1,3 +1,44 @@
-# frozen_string_literal = true
+# frozen_string_literal: true
 
 require_relative '../lib/email_valdoise'
+
+describe "should return a link to the town infos" do
+  it "should return an array" do
+    expect(link_fetcher.class).to eq(Array)
+  end
+
+  it "should end with .html" do
+    expect(link_fetcher[0].include? ".html").to eq(true)
+  end
+end
+
+describe "should fetch the e-mail adress of each town" do
+  it "should contain a @" do
+    expect(mail_fetcher[0].include? "@").to eq(true)
+  end
+
+  it "should have at least one dot" do
+    expect(mail_fetcher[0].include? ".").to eq(true)
+  end
+end
+
+describe "should fetch town names" do
+  it "should return an array" do
+    expect(name_fetcher.class).to eq(Array)
+  end
+
+  it "should be Ableiges at index 0" do
+    expect(name_fetcher[0]).to eq("Ableiges")
+  end
+end
+
+describe "should return a hash" do
+  it "should be a hash" do
+    expect(data_gatherer.class).to eq(Hash)
+  end
+
+  it "should have key Ableiges with good value" do
+    expect(data_gatherer.has_key?("Ableiges")).to eq(true)
+    expect(data_gatherer["Ableiges"]).to eq("mairie.ableiges95@wanadoo.fr")
+  end
+end
