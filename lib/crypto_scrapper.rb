e3207123428ecfaf4cb47f92ff392f9bcbae08ba
Salome Marzin <salome.mrzn@gmail.com>
2019-01-17 15:10:49 +0100

    crypto_scrapper.rb completed

--- lib/crypto_scrapper.rb
+++ lib/crypto_scrapper.rb
@@ -2,3 +2,33 @@
 
 require 'pry'
 require 'nokogiri'
+require 'open-uri'
+
+def page
+page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))   
+end
+
+def get_money_name
+  money_name = page.xpath('//td[@class="text-left col-symbol"]').to_a
+end
+
+def get_money_value
+  money_value = page.xpath('//a[@class="price"]').to_a
+end
+
+def transform_to_text
+  money_name_text = []  
+    get_money_name.each do |name|
+        name = name.text.to_sym
+        money_name_text.push(name)
+    end
+    return money_name_text
+end
+
+def transform_to_float
+    get_money_value.map!{|value| value.text.to_s.slice!(1..value.text.to_s.length).to_f}
+end
+
+def crypto_gatherer
+    hash = transform_to_text.zip(transform_to_float).to_h
+end
--- spec/crypto_scrapper_spec.rb
+++ spec/crypto_scrapper_spec.rb
@@ -1,3 +1,17 @@
 # frozen_string_literal = true
 
-require_relative '../lib/ruby-scrapper'
+require_relative '../lib/crypto_scrapper'
+
+describe "it should gather all cryptomoney into a hash" do
+  it "return hash" do
+    expect(crypto_gatherer.class).to eq(Hash)
+  end
+
+  it "should include bitcoin" do
+    expect(crypto_gatherer.has_key?(:BTC)).to eq(true)
+  end
+
+  it "should at least have 2109 elements" do
+    expect(crypto_gatherer.length).to be_between(2000, 2300).inclusive 
+  end
+end
